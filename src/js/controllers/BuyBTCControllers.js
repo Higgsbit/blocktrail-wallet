ea9cfb8cf88d251bfb582f5f5db2f66c81666d1c
Ruben de Vries <ruben@rubensayshi.com>
2016-09-14 15:32:48 +0200

    WIP

--- src/js/controllers/BuyBTCControllers.js
+++ src/js/controllers/BuyBTCControllers.js
@@ -1,6 +1,12 @@
 angular.module('blocktrail.wallet')
     .controller('WalletBuyBTCCtrl', function($q, $scope, $cordovaDialogs, $ionicLoading, $translate, Wallet) {
-        $scope.goGlidera = function() {
+        var bitcoin = blocktrailSDK.bitcoin;
+
+        var encodeURIComponentBitIDURI = function(bitidUri) {
+            return "bitid://" + encodeURIComponent(bitidUri.replace("bitid://", ""));
+        };
+
+        $scope.goGlideraBrowser = function() {
             var clientId = "9074010d6e573bd7b06645735ba315c8";
             var returnuri = "btccomwallet://glideraCallback";
             var glideraUrl = "https://sandbox.glidera.io/register?&client_id=" + clientId + "&redirect_uri=" + returnuri + "&response_type=code";
@@ -11,37 +17,58 @@ angular.module('blocktrail.wallet')
         };
 
         $scope.goGlideraBitID = function() {
+            var hdHighBit = 0x80000000;
+            var glideraHdPath = "m/" + (hdHighBit - 1) +  "'/0'";
             var clientId = "9074010d6e573bd7b06645735ba315c8";
-            var returnuri = "btccomwallet://glideraCallback";
-            var glideraUrl = "https://sandbox.glidera.io/register?&client_id=" + clientId + "&redirect_uri=" + returnuri + "&response_type=code";
+            var returnuri = "urn:ietf:wg:oauth:2.0:oob"; // "btccomwallet://glideraCallback";
 
-            return $cordovaDialogs.prompt(
-                $translate.instant('MSG_ENTER_PIN').sentenceCase(),
-                $translate.instant('SETUP_GLIDERA').sentenceCase(),
-                    [$translate.instant('OK'), $translate.instant('CANCEL').sentenceCase()],
-                    "",
-                    true,   //isPassword
-                    "tel"   //input type (uses html5 style)
-                )
-                .then(function(dialogResult) {
-                    if (dialogResult.buttonIndex == 2) {
-                        return $q.reject('CANCELLED');
-                    }
-                    //decrypt password with the provided PIN
-                    $ionicLoading.show({template: "<div>{{ 'WORKING' | translate }}...</div><ion-spinner></ion-spinner>", hideOnStateChange: true});
+            // return $cordovaDialogs.prompt(
+            //     $translate.instant('MSG_ENTER_PIN').sentenceCase(),
+            //     $translate.instant('SETUP_GLIDERA').sentenceCase(),
+            //         [$translate.instant('OK'), $translate.instant('CANCEL').sentenceCase()],
+            //         "",
+            //         true,   //isPassword
+            //         "tel"   //input type (uses html5 style)
+            //     )
+            //     .then(function(dialogResult) {
+            //         if (dialogResult.buttonIndex == 2) {
+            //             return $q.reject('CANCELLED');
+            //         }
+            //         //decrypt password with the provided PIN
+            //         $ionicLoading.show({template: "<div>{{ 'WORKING' | translate }}...</div><ion-spinner></ion-spinner>", hideOnStateChange: true});
+            //
+            //         return Wallet.unlock(dialogResult.input1).then(function(unlockData) {
+            //             $ionicLoading.hide();
+            //
+            //             return unlockData;
+            //         });
+            //     })
+            //
+                return Wallet.unlock("0000")
 
-                    return Wallet.unlock(dialogResult.input1).then(function(unlockData) {
-                        $ionicLoading.hide();
-
-                        return unlockData;
-                    });
-                })
                 .then(function(wallet) {
-                    return wallet.getNewAddress().then(function(address) {
-                        alert(address);
-                    });
+                    var privKey = blocktrailSDK.Wallet.deriveByPath(wallet.primaryPrivateKey, glideraHdPath, "m/");
+                    var address = privKey.getAddress().toBase58Check();
+
+                    var bitidUri = "bitid://" + "sandbox.glidera.io/bitid/auth?x=" + Math.ceil((new Date).getTime() / 1000);
+                    var bitidSig = bitcoin.Message.sign(privKey.privKey, bitidUri).toString('base64');
+
+
+                    var qs = [
+                        'client_id=' + clientId,
+                        'redirect_uri=' + encodeURIComponent(returnuri),
+                        'bitid_address=' + address,
+                        'bitid_signature=' + encodeURIComponent(bitidSig),
+                        'bitid_uri=' + encodeURIComponentBitIDURI(bitidUri)
+                    ];
+
+                    var glideraUrl = "https://sandbox.glidera.io/bitid/auth?" + qs.join("&");
+
+                    console.log(glideraUrl);
                 });
         };
+
+        $scope.goGlideraBitID();
     }
 );
 
